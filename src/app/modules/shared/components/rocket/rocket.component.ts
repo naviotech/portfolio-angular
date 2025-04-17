7eaa7700463b3e5b8e138ff36fba32eb5a6d1014
Eduardo Navío <naviomaya@gmail.com>
2025-04-17 11:56:34 +0200

    add bird animation

--- src/app/modules/shared/components/rocket/rocket.component.ts
+++ src/app/modules/shared/components/rocket/rocket.component.ts
@@ -10,6 +10,7 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class RocketComponent implements OnInit {
   clouds: any[] = [];
+  smokeTrail: any[] = [];
 
   ngOnInit(): void {
     this.generateClouds(10);
@@ -18,22 +19,18 @@ export class RocketComponent implements OnInit {
 
   generateClouds(count: number): void {
     this.clouds = Array.from({ length: count }, () => ({
-      scale: (Math.random() * 0.6 + 0.7).toFixed(2), // 0.7 - 1.3
-      opacity: (Math.random() * 0.5 + 0.5).toFixed(2), // 0.5 - 1
-      delay: `${(Math.random() * 3).toFixed(2)}s`, // 0 - 3s
-      duration: `${(Math.random() * 3 + 2).toFixed(2)}s`, // 2 - 5s
+      scale: (Math.random() * 0.6 + 0.7).toFixed(2),
+      opacity: (Math.random() * 0.5 + 0.5).toFixed(2),
+      delay: `${(Math.random() * 3).toFixed(2)}s`,
+      duration: `${(Math.random() * 3 + 2).toFixed(2)}s`,
       left: `${Math.random() * 100}%`,
     }));
   }
-
-
-  smokeTrail: any[] = [];
-
   generateSmokeTrail(count: number) {
     this.smokeTrail = Array.from({ length: count }, (_, i) => ({
-      x: Math.random() * 20 - 10, // ligera variación horizontal
-      delay: i * 0.2,             // separación en el tiempo
-      scale: (Math.random() * 0.5 + 0.8).toFixed(2)
+      x: Math.random() * 20 - 10,
+      delay: i * 0.2,
+      scale: (Math.random() * 0.5 + 0.8).toFixed(2),
     }));
   }
 }
